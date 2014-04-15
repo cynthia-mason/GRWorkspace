4fcef3f3f42e14f2f430fb79644d36c5d4ef97b5
cynthia-mason <cynthia.mason@outlook.com>
2014-04-15 15:14:31 -0400

    Trying to work on Scratch

--- GRScratch/Debug/Function_PCG_WoodScratch.d
+++
@@ -1,8 +0,0 @@
-Function_PCG_WoodScratch.d: ../Function_PCG_WoodScratch.c \
- ../blas_sparse.h ../blas_enum.h ../blas_sparse_proto.h
-
-../blas_sparse.h:
-
-../blas_enum.h:
-
-../blas_sparse_proto.h:
