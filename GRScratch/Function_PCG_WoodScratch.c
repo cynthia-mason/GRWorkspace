6a9f056813e31903ade0d28dc91924d8b8650687
cynthia-mason <cynthia.mason@outlook.com>
2014-04-16 11:32:01 -0400

    Added up to creating cool_row_v

--- GRScratch/Function_PCG_WoodScratch.c
+++ GRScratch/Function_PCG_WoodScratch.c
@@ -2,6 +2,7 @@
 #include "blas_sparse.h"
 #include "stdio.h"
 
+int BASE = 1;
 void Function_PCG_WoodScratch(
 		const int numRowA, const int numColA,const int numNZA,
 		double A_val[29], int A_indx[29], int A_jndx[29],
@@ -42,8 +43,10 @@ void Function_PCG_WoodScratch(
 
 	/*	INITIALIZE	*/
 	blas_sparse_matrix Minv;
-	int ind;
-	int row[numColA];
+	int test_true, bool_row_v;
+	double row[numColA], dx_1[numColA];
+	double sum=0, epsilon;
+
 
 	//CONSTRUCT A
 	blas_sparse_matrix A;
@@ -53,22 +56,50 @@ void Function_PCG_WoodScratch(
 	A=BLAS_duscr_begin(numRowA,numColA);
 
 	/*  Step 2: Insert entries  */
-	for (i=0;i<numNZA;i++)
+	for (i=0;i<numNZA;i++){
 		BLAS_duscr_insert_entry(A,A_val[i],A_indx[i],A_jndx[i]);
+		//printf("At i = %d, and j = %d ==> A[%d] = %f \n",A_indx[i], A_jndx[i], i,A_val[i]);
+	}
 
 	/*  Step 3: Complete construction of sparse matrix  */
 	BLAS_duscr_end(A);
 
-	i=0;
-	ind=0;
-	while(i<numColA){
-		if(A_indx[ind]==0){
-			row[i] = A_val[ind];
-			i++;
-			printf("row[%d] = %d \n",i,row[i]);
+
+	/* generate "row" and sum row for creation of "epsilon"*/
+	for(i=0;i<numColA;i++)
+		row[i]=0.0;
+
+
+	test_true = 0+BASE;
+	for(i=0;i<numNZA;i++){
+		if(A_indx[i]==test_true){
+			row[A_jndx[i]-BASE] = A_val[i];
+			sum+=A_val[i];
 		}
-		ind++;
 	}
+	epsilon = 1/(2*sum);
+	/*-----------------*/
+
+	for(i=0;i<numColA;i++){
+		x[i] = epsilon;
+		dx_1[i] = epsilon;
+	}
+
+	*NumVar = numColA;
+
+	//Find where the components of A begin and end [A_tree;A_edge;A_nonneg]
+	//1) The Tree constraints end
+	test_true = numColA+BASE;
+	bool_row_v = numRowA;
+	for(i=0;i<numNZA;i++){
+		// If the element is in the last column and has the smallest number row
+		if(A_jndx[i]==test_true && A_indx[i]<bool_row_v){
+			bool_row_v = A_indx[i];
+		}
+	}
+	printf()
+	//array123_row_v=1:1:numel(bool_row_v);
+	//row_v=array123_row_v(bool_row_v);//Find the last row of net constratints (A must be constructed such that there are blocks of cascading ones)
 
 //	/*  Step 4: Compute matrix vector product y=A*x  */
 //	BLAS_dusmv(blas_no_trans,alpha,A,x,1,y,1);
