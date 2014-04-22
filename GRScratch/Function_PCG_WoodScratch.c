3651e6fa00a91dacbfc80381fc2b0ef2b3030549
cmason01 <cmason01@SOE-CMC-HP02.cfs.uoguelph.ca>
2014-04-22 10:01:01 -0400

    Changes made to split input into respective tree track and nonneg

--- GRScratch/Function_PCG_WoodScratch.c
+++ GRScratch/Function_PCG_WoodScratch.c
@@ -2,13 +2,15 @@
 #include "blas_sparse.h"
 #include "stdio.h"
 
-int BASE = 1;
+int BASE = 0;
 void Function_PCG_WoodScratch(
 		const int numRowA_tree,const int numRowA_track,const int numRowA_nonneg,
 		const int numColA,const int numNZA_tree,const int numNZA_track,const int numNZA_nonneg,
-		double A_tree_val[29], int A_tree_indx[29], int A_tree_jndx[29],
-		double A_track_val[29], int A_track_indx[29], int A_track_jndx[29],
-		double A_nonneg_val[29], int A_nonneg_indx[29], int A_nonneg_jndx[29],
+		double A_val[29], int A_indx[29], int A_jndx[29],
+		double A_tree_val[10], int A_tree_indx[10], int A_tree_jndx[10],
+		double A_track_val[9], int A_track_indx[9], int A_track_jndx[9],
+		double A_nonneg_val[10], int A_nonneg_indx[10], int A_nonneg_jndx[10],
+		double b_tree[3],double b_track[4],double b_nonneg[10],
 		double b[17], double c[10], double gamma,
 		double x[10], int *NumVar, int *IPMit, double *CondNum, int *PCGit){
 
@@ -45,46 +47,67 @@ void Function_PCG_WoodScratch(
 		printf("%f, ",y[i]);*/
 
 	/*	INITIALIZE	*/
+	blas_sparse_matrix A;
 	blas_sparse_matrix A_tree;
 	blas_sparse_matrix A_track;
 	blas_sparse_matrix A_nonneg;
+/*	blas_sparse_matrix D_treeBLAS;
+	blas_sparse_matrix D_trackBLAS;
+	blas_sparse_matrix D_nonnegBLAS;*/
+	double D_treeBLAS[numRowA_tree][numRowA_tree];
+	double D_trackBLAS[numRowA_track][numRowA_track];
+	double D_nonnegBLAS[numRowA_nonneg][numRowA_nonneg];
 	blas_sparse_matrix Minv;
 	int test_true;
-	double row[numColA], dx_1[numColA];
+	int numRowA=numRowA_tree+numRowA_track+numRowA_nonneg;
+	double row[numColA], dx_1[numColA],w_temp[numRowA],w[numRowA];
+	double w_temp_tree[numRowA_tree],w_temp_track[numRowA_track],w_temp_nonneg[numRowA_nonneg];
+	double w_tree[numRowA_tree], D_tree[numRowA_tree], w_track[numRowA_track], D_track[numRowA_track], w_nonneg[numRowA_nonneg], D_nonneg[numRowA_nonneg];
+	int D_tree_indices[numRowA_tree],D_track_indices[numRowA_track],D_nonneg_indices[numRowA_nonneg];
 	double sum=0, epsilon;
-	int bool_row_v[numRowA], array123_row_v[numRowA];
-	int row_v[numRowA], row_v_ind;
-
-	//CONSTRUCT A's
-	blas_sparse_matrix A_tree;
-	blas_sparse_matrix A_track;
-	blas_sparse_matrix A_nonneg;
+	int check;
 	int i;
+	double D[numColA];
+
+	//double x_test[9]={1,1,1,1,1,1,1,1,1}, w_test[9]={0,0,0,0,0,0,0,0,0};
+
+
+	// ------------- CONSTRUCT A's -------------
+
 
 	/*  Step 1: Create Spares BLAS Handle  */
+	A=BLAS_duscr_begin(numRowA,numColA);
 	A_tree=BLAS_duscr_begin(numRowA_tree,numColA);
 	A_track=BLAS_duscr_begin(numRowA_track,numColA);
 	A_nonneg=BLAS_duscr_begin(numRowA_nonneg,numColA);
 
+	//Wanted to have sparse diagonal matrices, but BLAS won't allow multiplication between 2 sparse matrices.
+/*	D_treeBLAS=BLAS_duscr_begin(numRowA_tree,numRowA_tree);
+	D_trackBLAS=BLAS_duscr_begin(numRowA_track,numRowA_track);
+	D_nonnegBLAS=BLAS_duscr_begin(numRowA_nonneg,numRowA_nonneg);*/
+
 	/*  Step 2: Insert entries  */
+	for (i=0;i<(numNZA_tree+numNZA_track+numNZA_nonneg);i++){
+			BLAS_duscr_insert_entry(A,A_val[i],A_indx[i],A_jndx[i]);
+			//printf("At i = %d, and j = %d ==> A[%d] = %f \n",A_indx[i], A_jndx[i], i,A_val[i]);
+	}
 	for (i=0;i<numNZA_tree;i++){
 		BLAS_duscr_insert_entry(A_tree,A_tree_val[i],A_tree_indx[i],A_tree_jndx[i]);
 		//printf("At i = %d, and j = %d ==> A[%d] = %f \n",A_indx[i], A_jndx[i], i,A_val[i]);
 	}
 	for (i=0;i<numNZA_track;i++){
 		BLAS_duscr_insert_entry(A_track,A_track_val[i],A_track_indx[i],A_track_jndx[i]);
-		//printf("At i = %d, and j = %d ==> A[%d] = %f \n",A_indx[i], A_jndx[i], i,A_val[i]);
 	}
 	for (i=0;i<numNZA_nonneg;i++){
 		BLAS_duscr_insert_entry(A_nonneg,A_nonneg_val[i],A_nonneg_indx[i],A_nonneg_jndx[i]);
-		//printf("At i = %d, and j = %d ==> A[%d] = %f \n",A_indx[i], A_jndx[i], i,A_val[i]);
 	}
 
 	/*  Step 3: Complete construction of sparse matrix  */
+	BLAS_duscr_end(A);
 	BLAS_duscr_end(A_tree);
 	BLAS_duscr_end(A_track);
 	BLAS_duscr_end(A_nonneg);
-
+	// -------------               -------------
 
 	/* generate "row" and sum row for creation of "epsilon"*/
 	for(i=0;i<numColA;i++)
@@ -108,66 +131,85 @@ void Function_PCG_WoodScratch(
 
 	*NumVar = numColA;
 
-	//------GOOD TILL HERE!!
+	printf("Epsilon: %f",epsilon);
 
-	//Find where the components of A begin and end [A_tree;A_edge;A_nonneg]
-	//1) The Tree constraints end
-	test_true = numColA-1+BASE;
-//	for(i=0;i<numRowA;i++){
-//		bool_row_v[i]=0;
-//		array123_row_v[i]=i;
-//	}
-//
-//	for(i=0;i<numNZA;i++){
-//		// If the element is in the last column and has the smallest number row
-//		if(A_jndx[i]==test_true && A_val[i]==1){
-//			printf("A_jndx[%d] = %d, A_val[%d] = %f, \n",i,A_jndx[i],i,A_val[i]);
-//			bool_row_v[A_indx[i]-BASE] = 1;
-//			printf("bool_row_v[%d] = %d, \n",A_indx[i],bool_row_v[A_indx[i]-BASE]);
-//		}
-//	}
-//	printf("bool_row_v:\n");
-//	for(i=0;i<numRowA;i++)
-//		printf("%d ",bool_row_v[i]);
 
-	for(i=0;i<numRowA;i++){
-		row_v[i]=-1; //THIS IS a FLAG SO THAT IT WILL NOT BE USED AN INDICES
-	}
-	row_v_ind=0;
-	for(i=0;i<numNZA;i++){
-		// If the element is in the last column and has the smallest number row
-		if(A_jndx[i]==test_true && A_val[i]==1){
-			row_v[row_v_ind] = A_indx[i]-1;
-			printf("A_jndx[%d] = %d, A_val[%d] = %f, \n",i,A_jndx[i],i,A_val[i]);
-			printf("bool_row_v[%d] = %d, \n",row_v_ind,row_v[row_v_ind]);
-			row_v_ind++;
+	// ----------- CALCULATIONS ------------
+
+//	for(check=0;check<20;check++){
+		printf("\n--- Interior point iteration: %d ---\n",check);
+
+		//Step 2
+		for(i=0;i<numRowA;i++){
+			w_temp[i] = 0.0;
 		}
-	}
 
-	printf("row_v:\n");
-	for(i=0;i<numRowA;i++)
-		printf("%d ",row_v[i]);
+		BLAS_dusmv(blas_no_trans,1.0,A_tree,x,1,w_temp_tree,1);
+		BLAS_dusmv(blas_no_trans,1.0,A_track,x,1,w_temp_track,1);
+		BLAS_dusmv(blas_no_trans,1.0,A_nonneg,x,1,w_temp_nonneg,1);
+		//int BLAS_dusmv( enum blas_trans_type transa, double alpha,blas_sparse_matrix A, const double *x, int incx, double *y, int incy );
 
-	A_tree=BLAS_duscr_begin(row_v[1]+1,numColA);
-	for (i=0;i<numNZA_tree;i++){
-		BLAS_duscr_insert_entry(A,A_val[i],A_indx[i],A_jndx[i]);
-		//printf("At i = %d, and j = %d ==> A[%d] = %f \n",A_indx[i], A_jndx[i], i,A_val[i]);
-	}
-	/*  Step 3: Complete construction of sparse matrix  */
-	BLAS_duscr_end(A);
-	/* generate "row" and sum row for creation of "epsilon"*/
-	for(i=0;i<numColA;i++)
-		row[i]=0.0;
-	test_true = 0+BASE;
-	for(i=0;i<numNZA;i++){
-		if(A_indx[i]==test_true){
-			row[A_jndx[i]-BASE] = A_val[i];
-			sum+=A_val[i];
+		for(i=0;i<numRowA_tree;i++){
+			w_tree[i]=b_tree[i]-w_temp_tree[i];
+			D_tree[i]=1/w_tree[i];
+			D_tree_indices[i]=i;
 		}
-	}
-	epsilon = 1/(2*sum);
-	/*-----------------*/
+		for(i=0;i<numRowA_track;i++){
+			w_track[i]=b_track[i]-w_temp_track[i];
+			D_track[i]=1/w_track[i];
+			D_track_indices[i]=i;
+		}
+		for(i=0;i<numRowA_nonneg;i++){
+			w_nonneg[i]=b_nonneg[i]-w_temp_nonneg[i];
+			D_nonneg[i]=1/w_nonneg[i];
+			D_nonneg_indices[i]=i;
+		}
 
+	/*	//Construct Diagonal matrices
+		for (i=0;i<numRowA_tree;i++){
+				BLAS_duscr_insert_entry(D_treeBLAS,D_tree[i],D_tree_indices[i],D_tree_indices[i]);
+				//printf("At i = %d, and j = %d ==> A[%d] = %f \n",A_indx[i], A_jndx[i], i,A_val[i]);
+		}
+		for (i=0;i<numRowA_track;i++){
+			BLAS_duscr_insert_entry(D_trackBLAS,D_track[i],D_track_indices[i],D_track_indices[i]);
+		}
+		for (i=0;i<numRowA_nonneg;i++){
+			BLAS_duscr_insert_entry(D_nonnegBLAS,D_nonneg[i],D_nonneg_indices[i],D_nonneg_indices[i]);
+		}
+
+		BLAS_duscr_end(D_treeBLAS);
+		BLAS_duscr_end(D_trackBLAS);
+		BLAS_duscr_end(D_nonnegBLAS);*/
+
+		for (i=0;i<numRowA_tree;i++){
+			for (i=0;i<numRowA_tree;i++){
+
+			}
+		}
+		for (i=0;i<numRowA_track;i++){
+		}
+		for (i=0;i<numRowA_nonneg;i++){
+		}
+
+		//ADDA=(A_tree')*D_tree*D_tree*A_tree
+		//			+(A_track')*D_track*D_track*A_track
+		//			+(A_nonneg')*D_nonneg*D_nonneg*A_nonneg;
+
+		/*------CONSTRUCT ADDA------*/
+
+		//(A_tree')*D_tree*D_tree*A_tree
+/*
+		printf("w:     w_temp:       D:\n");
+		for(i=0;i<numRowA;i++){
+			w[i]=b[i]-w_temp[i];
+			D[i]=1/w[i];
+			printf("%lf , %lf , %lf\n",w[i], w_temp[i],D[i]);
+		}
+*/
+
+
+
+//	}
 
 //	/*  Step 4: Compute matrix vector product y=A*x  */
 //	BLAS_dusmv(blas_no_trans,alpha,A,x,1,y,1);
--- GRScratch/testRun_Function_PCG_WoodScratch.c
+++ GRScratch/testRun_Function_PCG_WoodScratch.c
@@ -6,14 +6,63 @@
 
   int main(){
   
-  const int numRowA=17;	//Pass in
+  const int numRowA_tree=3;	//Pass in
+  const int numRowA_track=4;	//Pass in
+  const int numRowA_nonneg=10;	//Pass in
+
   const int numColA=10;	//Pass in
-  const int numNZA=29;	//Pass in
-  double A_val[29] = {1, 1, -1, 1, 1, -1, 1, 1, -1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, -1};	//Pass in
-  int A_indx[29] ={1, 4, 8, 1, 5, 9, 1, 7, 10, 1, 11, 2, 4, 12, 2, 5, 13, 2, 6, 14, 3, 4, 15, 3, 7, 16, 3, 6, 17};	//Pass in
-  int A_jndx[29] = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10};	//Pass in
-  double b[17] = {1,1,1,2,2,1,1,0,0,0,0,0,0,0,0,0,0};	//Pass in
-  double c[10] ={10,8,6,4,5,4,13,16,7,8};
+
+  const int numNZA_tree=10;	//Pass in
+  const int numNZA_track=9;	//Pass in
+  const int numNZA_nonneg=10;	//Pass in
+
+
+  double A_tree_val[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};	//Pass in
+  int A_tree_indx[] ={1, 1, 1, 1, 2, 2, 2, 3, 3, 3};	//Pass in
+  int A_tree_jndx[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};	//Pass in
+
+  double A_track_val[] = {1, 1, 1, 1, 1, 1, 1, 1, 1};	//Pass in
+  int A_track_indx[] ={1, 2, 4, 1, 2, 3, 1, 4, 3};	//Pass in
+  int A_track_jndx[] = {1, 2, 3, 5, 6, 7, 8, 9, 10};	//Pass in
+
+  double A_nonneg_val[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};	//Pass in
+  int A_nonneg_indx[] ={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};	//Pass in
+  int A_nonneg_jndx[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};	//Pass in
+
+  double A_val[] = {1,1,-1,1,1,-1,1,1,-1,1,-1,1,1,-1,1,1,-1,1,1,-1,1,1,-1,1,1,-1,1,1,-1};	//Pass in
+  int A_indx[] ={1,4,8,1,5,9,1,7,10,1,11,2,4,12,2,5,13,2,6,14,3,4,15,3,7,16,3,6,17};	//Pass in
+  int A_jndx[] = {1,1,1,2,2,2,3,3,3,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9,10,10,10};	//Pass in
+
+  int i=0;
+
+  for(i=0;i<10;i++){
+	  A_tree_indx[i]-=1;
+	  A_tree_jndx[i]-=1;
+	  A_nonneg_indx[i]-=1;
+	  A_nonneg_jndx[i]-=1;
+  }
+  for(i=0;i<9;i++){
+	  A_track_indx[i]-=1;
+	  A_track_jndx[i]-=1;
+  }
+  for(i=0;i<29;i++){
+	  A_indx[i]-=1;
+	  A_jndx[i]-=1;
+  }
+
+/*
+    double A_val[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1};	//Pass in
+    int A_indx[9] ={1, 2, 3, 4, 5, 6, 7, 8, 9};	//Pass in
+    int A_jndx[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};	//Pass in
+*/
+
+
+
+  double b[] = {1,1,1,2,2,1,1,0,0,0,0,0,0,0,0,0,0};	//Pass in
+  double b_tree[] = {1,1,1};	//Pass in
+  double b_track[] = {2,2,1,1};	//Pass in
+  double b_nonneg[] = {0,0,0,0,0,0,0,0,0,0};	//Pass in
+  double c[] ={10,8,6,4,5,4,13,16,7,8};
   double gamma = 0.9;	//Pass in
   double x[10];
   int* NumVar;
@@ -21,7 +70,7 @@
   int* CondNum;
   int* PCGit;
 
-  int i;
+
   
   //Allocate an int pointee
   NumVar = malloc(sizeof(int));
@@ -29,7 +78,14 @@
   CondNum = malloc(sizeof(int));
   PCGit = malloc(sizeof(int));
 
-  Function_PCG_WoodScratch(numRowA, numColA, numNZA, A_val, A_indx, A_jndx, b, c, gamma, x, NumVar, IPMit, CondNum, PCGit);
+  Function_PCG_WoodScratch(numRowA_tree,numRowA_track,numRowA_nonneg, numColA,
+		  numNZA_tree,numNZA_track,numNZA_nonneg,
+		  A_val, A_indx, A_jndx,
+		  A_tree_val, A_tree_indx, A_tree_jndx,
+		  A_track_val, A_track_indx, A_track_jndx,
+		  A_nonneg_val, A_nonneg_indx, A_nonneg_jndx,
+		  b_tree,b_track,b_nonneg,
+		  b, c, gamma, x, NumVar, IPMit, CondNum, PCGit);
   //Function_PCG_WoodScratch(const int numRowA, const int numColA, const int numNZA, double[] Aval, int[] A_indx, int[] A_jndx, double[] b, double[] c, double gamma, double[] x, int *NumVar, int *IPMit, double *CondNum, int *PCGit)
 
   printf("\n\n x = \n");
